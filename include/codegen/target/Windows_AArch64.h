fdb7a7c232424d3387c18217a9ea3de87d7d18a0
Netesy [Maximus] Emmanuel <netesy@users.noreply.github.com>
2026-04-21 07:53:51 +0400

    Implement full Windows AArch64 capability domain target

--- include/codegen/target/Windows_AArch64.h
+++ include/codegen/target/Windows_AArch64.h
@@ -18,6 +18,27 @@ public:
     void emitRem(CodeGen& cg, ir::Instruction& instr) override;
     void emitSyscall(CodeGen& cg, ir::Instruction& instr) override;
     uint64_t getSyscallNumber(ir::SyscallId id) const override;
+    bool supportsCapability(const CapabilitySpec& spec) const override;
+    void emitIOCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitFSCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitMemoryCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitProcessCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitThreadCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitSyncCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitTimeCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitEventCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitNetCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitIPCCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitEnvCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitSystemCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitSignalCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitRandomCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitErrorCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitDebugCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitModuleCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitTTYCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitSecurityCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
+    void emitGPUCapability(CodeGen&, ir::Instruction&, const CapabilitySpec&) override;
 };
 
 } // namespace target
--- src/codegen/target/Windows_AArch64.cpp
+++ src/codegen/target/Windows_AArch64.cpp
@@ -92,5 +92,109 @@ void Windows_AArch64::emitSyscall(CodeGen& cg, ir::Instruction& instr) {
     }
 }
 
+namespace {
+void emitWinA64Arg(CodeGen& cg, ir::Instruction& instr, size_t idx, const char* reg, std::ostream& os) {
+    if (idx < instr.getOperands().size()) {
+        os << "  mov " << reg << ", " << cg.getValueAsOperand(instr.getOperands()[idx]->get()) << "\n";
+    } else {
+        os << "  mov " << reg << ", #0\n";
+    }
+}
+void emitWinA64StoreResult(CodeGen& cg, ir::Instruction& instr, std::ostream& os) {
+    if (instr.getType()->getTypeID() != ir::Type::VoidTyID) {
+        os << "  str x0, " << cg.getValueAsOperand(&instr) << "\n";
+    }
+}
+}
+
+bool Windows_AArch64::supportsCapability(const CapabilitySpec& spec) const {
+    return spec.id != CapabilityId::GPU_COMPUTE;
+}
+
+void Windows_AArch64::emitIOCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream();
+    if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    switch (spec.id) {
+        case CapabilityId::IO_READ: emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); emitWinA64Arg(cg, instr, 2, "x2", *os); *os << "  bl ReadFile\n"; break;
+        case CapabilityId::IO_WRITE: emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); emitWinA64Arg(cg, instr, 2, "x2", *os); *os << "  bl WriteFile\n"; break;
+        case CapabilityId::IO_OPEN: emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl CreateFileA\n"; break;
+        case CapabilityId::IO_CLOSE: emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl CloseHandle\n"; break;
+        case CapabilityId::IO_SEEK: emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); emitWinA64Arg(cg, instr, 2, "x2", *os); *os << "  bl SetFilePointerEx\n"; break;
+        case CapabilityId::IO_STAT: emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); *os << "  bl GetFileInformationByHandle\n"; break;
+        case CapabilityId::IO_FLUSH: emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl FlushFileBuffers\n"; break;
+        default: return emitUnsupportedCapability(cg, instr, &spec);
+    }
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitFSCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    if (spec.id == CapabilityId::FS_OPEN || spec.id == CapabilityId::FS_CREATE) return emitIOCapability(cg, instr, CapabilitySpec{CapabilityId::IO_OPEN, "io.open", CapabilityDomain::IO, 2, 3, true, true});
+    auto* os = cg.getTextStream();
+    if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::FS_STAT) { emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); *os << "  bl GetFileAttributesExA\n"; }
+    else if (spec.id == CapabilityId::FS_REMOVE) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl DeleteFileA\n"; }
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitMemoryCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream();
+    if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::MEMORY_ALLOC || spec.id == CapabilityId::MEMORY_MAP) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl VirtualAlloc\n"; }
+    else if (spec.id == CapabilityId::MEMORY_FREE) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl VirtualFree\n"; }
+    else if (spec.id == CapabilityId::MEMORY_PROTECT) { emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); emitWinA64Arg(cg, instr, 2, "x2", *os); *os << "  bl VirtualProtect\n"; }
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitProcessCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream();
+    if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::PROCESS_EXIT) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl ExitProcess\n"; }
+    else if (spec.id == CapabilityId::PROCESS_ABORT) { *os << "  bl abort\n"; }
+    else if (spec.id == CapabilityId::PROCESS_SLEEP) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl Sleep\n"; }
+    else if (spec.id == CapabilityId::PROCESS_SPAWN) { emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); *os << "  bl CreateProcessA\n"; }
+    else if (spec.id == CapabilityId::PROCESS_ARGS) { *os << "  bl GetCommandLineA\n"; }
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitThreadCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream(); if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::THREAD_SPAWN) { emitWinA64Arg(cg, instr, 0, "x0", *os); emitWinA64Arg(cg, instr, 1, "x1", *os); *os << "  bl CreateThread\n"; }
+    else if (spec.id == CapabilityId::THREAD_JOIN) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl WaitForSingleObject\n"; }
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitSyncCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream(); if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::SYNC_MUTEX_LOCK || spec.id == CapabilityId::SYNC_MUTEX_UNLOCK) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl InterlockedExchange64\n"; }
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitTimeCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) {
+    auto* os = cg.getTextStream(); if (!os) return emitUnsupportedCapability(cg, instr, &spec);
+    if (spec.id == CapabilityId::TIME_NOW) *os << "  bl GetSystemTimeAsFileTime\n";
+    else if (spec.id == CapabilityId::TIME_MONOTONIC) *os << "  bl QueryPerformanceCounter\n";
+    else return emitUnsupportedCapability(cg, instr, &spec);
+    emitWinA64StoreResult(cg, instr, *os);
+}
+
+void Windows_AArch64::emitEventCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (spec.id != CapabilityId::EVENT_POLL) return emitUnsupportedCapability(cg, instr, &spec); if (auto* os = cg.getTextStream()) { emitWinA64Arg(cg, instr, 0, "x0", *os); *os << "  bl WaitForMultipleObjects\n"; emitWinA64StoreResult(cg, instr, *os);} }
+void Windows_AArch64::emitNetCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (auto* os = cg.getTextStream()) { switch(spec.id){case CapabilityId::NET_SOCKET:*os<<"  bl socket\n";break;case CapabilityId::NET_CONNECT:*os<<"  bl connect\n";break;case CapabilityId::NET_LISTEN:*os<<"  bl listen\n";break;case CapabilityId::NET_ACCEPT:*os<<"  bl accept\n";break;case CapabilityId::NET_SEND:*os<<"  bl send\n";break;case CapabilityId::NET_RECV:*os<<"  bl recv\n";break;default:return emitUnsupportedCapability(cg,instr,&spec);} emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitIPCCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (spec.id==CapabilityId::IPC_SEND) return emitIOCapability(cg,instr,CapabilitySpec{CapabilityId::IO_WRITE,"io.write",CapabilityDomain::IO,3,3,true,true}); if (spec.id==CapabilityId::IPC_RECV) return emitIOCapability(cg,instr,CapabilitySpec{CapabilityId::IO_READ,"io.read",CapabilityDomain::IO,3,3,true,true}); emitUnsupportedCapability(cg,instr,&spec); }
+void Windows_AArch64::emitEnvCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (auto* os = cg.getTextStream()) { if (spec.id==CapabilityId::ENV_GET) *os<<"  bl GetEnvironmentVariableA\n"; else if (spec.id==CapabilityId::ENV_LIST) *os<<"  bl GetEnvironmentStringsA\n"; else return emitUnsupportedCapability(cg,instr,&spec); emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitSystemCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (spec.id!=CapabilityId::SYSTEM_INFO) return emitUnsupportedCapability(cg,instr,&spec); if (auto* os=cg.getTextStream()){ emitWinA64Arg(cg,instr,0,"x0",*os); *os<<"  bl GetSystemInfo\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitSignalCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if (auto* os=cg.getTextStream()){ if(spec.id==CapabilityId::SIGNAL_SEND)*os<<"  bl GenerateConsoleCtrlEvent\n"; else if(spec.id==CapabilityId::SIGNAL_REGISTER)*os<<"  bl SetConsoleCtrlHandler\n"; else return emitUnsupportedCapability(cg,instr,&spec); emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitRandomCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::RANDOM_U64) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ *os<<"  bl BCryptGenRandom\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitErrorCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::ERROR_GET) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ *os<<"  bl GetLastError\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitDebugCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::DEBUG_LOG) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ emitWinA64Arg(cg,instr,0,"x0",*os); *os<<"  bl OutputDebugStringA\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitModuleCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::MODULE_LOAD) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ emitWinA64Arg(cg,instr,0,"x0",*os); *os<<"  bl LoadLibraryA\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitTTYCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::TTY_ISATTY) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ emitWinA64Arg(cg,instr,0,"x0",*os); *os<<"  bl GetConsoleMode\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitSecurityCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { if(spec.id!=CapabilityId::SECURITY_CHMOD) return emitUnsupportedCapability(cg,instr,&spec); if(auto* os=cg.getTextStream()){ emitWinA64Arg(cg,instr,0,"x0",*os); emitWinA64Arg(cg,instr,1,"x1",*os); *os<<"  bl _chmod\n"; emitWinA64StoreResult(cg,instr,*os);} }
+void Windows_AArch64::emitGPUCapability(CodeGen& cg, ir::Instruction& instr, const CapabilitySpec& spec) { emitUnsupportedCapability(cg, instr, &spec); }
+
 } // namespace target
 } // namespace codegen
